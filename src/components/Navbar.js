b2d0b2ade93a596e1f0969c6d3a91fa74bf703b4
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2023-06-20 18:51:48 -0400

    starting navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,16 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+	return (
+		<nav classNAme = "navbar">
+			<div className = "navbar-container">
+				<Link to = "/" className = "navbar-logo">
+					
+				</Link>
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar
 
