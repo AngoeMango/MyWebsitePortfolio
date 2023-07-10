8de7e98ff7fce6efe447ed31abef5a081826dce6
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2023-07-10 08:58:48 -0400

    good morning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,22 @@
-import React, {useState} from 'react'
 import './NavbarStyles.css'
 
 function Navbar() {
 	return (
 		<nav className = "navbar">
 			<a href = "/Home" className = "site-title">AngoeMango</a>
-			<ul>
-				<li><a href = "/Resume" title = "Professional Work History">Resume</a></li>
-				<li><a href = "/Contact" title = "Get in touch!">Contact</a></li>
-				<li><a href = "/About" title = "Personal Life and Biography">Angeleeca</a></li>
-				<div className = "dropdown">
-					<button className = "dropbutton">
-						Projects
-						<i className = "fa fa-caret-down"></i>
-					</button>
-					<ul className = "dropcontent">
-						<li><a href = "/CodingProjects">Coding Projects</a></li>
-						<li><a href = "/SewingProjects">Sewing Projects</a></li>
-					</ul>
-				</div>
-			</ul>
+			<a href = "/Resume" title = "Professional Work History">Resume</a>
+			<a href = "/Contact" title = "Get in touch!">Contact</a>
+			<a href = "/About" title = "Personal Life and Biography">Angeleeca</a>
+			<div className = "dropdown">
+				<button className = "dropbutton">
+					Projects
+					<i className = "fa fa-caret-down"></i>
+				</button>
+				<ul className = "dropcontent">
+					<a href = "/CodingProjects">Coding Projects</a>
+					<a href = "/SewingProjects">Sewing Projects</a>
+				</ul>
+			</div>
 		</nav>
 	)
 }
