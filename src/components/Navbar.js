1dce3842490efda0432132fe215b2306f39b2b7c
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2023-07-09 20:55:35 -0400

    tried background thingy ?/

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 function Navbar() {
 	return (
-		<nav classNAme = "navbar">
+		<nav className = "navbar">
 			<div className = "navbar-container">
 				<Link to = "/" className = "navbar-logo">
 					
@@ -13,4 +13,3 @@ function Navbar() {
 }
 
 export default Navbar
-
