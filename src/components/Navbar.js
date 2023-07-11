bd380b30e0d216a48032fd41f11cdcccb70c56b9
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2023-07-11 00:07:24 -0400

    slay

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,20 @@ import './NavbarStyles.css'
 function Navbar() {
 	return (
 		<nav className = "navbar">
-			<a href = "/Home" className = "site-title">AngoeMango</a>
-			<ul>
-				<li><a href = "/Resume" title = "Professional Work History">Resume</a></li>
-				<li><a href = "/Contact" title = "Get in touch!">Contact</a></li>
-				<li><a href = "/About" title = "Personal Life and Biography">Angeleeca</a></li>
-				<div className = "dropdown">
-					<button className = "dropbutton">
-						Projects
-						<i className = "fa fa-caret-down"></i>
-					</button>
-					<ul className = "dropcontent">
-						<li><a href = "/CodingProjects">Coding Projects</a></li>
-						<li><a href = "/SewingProjects">Sewing Projects</a></li>
-					</ul>
+			<a href = "/About" title = "Personal Life and Biography">Angeleeca</a>
+			<a href = "/Contact" title = "Get in touch!">Contact</a>
+			<a href = "/Resume" title = "Professional Work History">Resume</a>
+			<div className = "dropdown">
+				<button className = "dropbutton">
+					Projects
+					<i className = "fa fa-caret-down"></i>
+				</button>
+				<div className = "dropcontent">
+					<a href = "/CodingProjects">Coding Projects</a>
+					<a href = "/SewingProjects">Sewing Projects</a>
 				</div>
-			</ul>
+			</div>
+			<div href = "/Home" title = "Home" className = "site-title">AngoeMango</div>
 		</nav>
 	)
 }
