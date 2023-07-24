e90289de562ec10b9640fef507e84f9d6683e899
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2023-07-24 07:51:16 -0400

    reworking this entire this bcus i came up w a new idea

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,15 @@
 import './NavbarStyles.css'
+import windowslogo from './images/windows-0.png'
 
 function Navbar() {
 	return (
 		<nav className = "navbar">
-			<a href = "/About" title = "Personal Life and Biography">Angeleeca</a>
-			<a href = "/Contact" title = "Get in touch!">Contact</a>
-			<a href = "/Resume" title = "Professional Work History">Resume</a>
-			<div className = "dropdown">
-				<button className = "dropbutton">
-					Projects
-					<i className = "fa fa-caret-down"></i>
+			<div class = "topnav-left">
+				<button class = "button98">
+					<img src = {windowslogo} alt-text = "windowslogo"></img>
+					Angeleeca
 				</button>
-				<div className = "dropcontent">
-					<a href = "/CodingProjects">Coding Projects</a>
-					<a href = "/SewingProjects">Sewing Projects</a>
-				</div>
 			</div>
-			<div href = "/Home" title = "Home" className = "site-title">AngoeMango</div>
 		</nav>
 	)
 }
